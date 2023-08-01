f2bc0c2a5407b364a66491fe42cfa47883a3de8f
TiffanySeptember <137154346+TiffanySeptember@users.noreply.github.com>
2023-08-01 19:47:05 +0200

    completed challenge 2

--- scripts.js
+++ scripts.js
@@ -5,3 +5,12 @@ const user = "John";
 /* This is the lastname of the user*/ const surname = "Smith";
 
 console.log(user + " " + surname);
+
+/** (c) ACME Inc. 2010 */
+
+/*
+ * This is the date that a user created their account
+ */
+const date = "10/07/2014";
+
+console.log(date);
