2fdd42e827cfc3dbfbf70ef773b4e611975c34e3
TiffanySeptember <137154346+TiffanySeptember@users.noreply.github.com>
2023-08-02 13:50:01 +0200

    updated files

---
+++ challenge1.js
@@ -0,0 +1,11 @@
+//CHALLENGE 1
+
+/* This is the firstname of the user */
+
+const user = "John";
+
+/* This is the lastname of the user*/ const surname = "Smith";
+
+console.log(user + " " + surname);
+
+/** (c) ACME Inc. 2010 */
---
+++ challenge2.js
@@ -0,0 +1,6 @@
+/*
+ * This is the date that a user created their account
+ */
+const date = "10/07/2014";
+
+console.log(date);
